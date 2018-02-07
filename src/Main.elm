5f27fb553a8db1a1f9155216c1d264b4f8b43638
Jan Fornoff <jan.fornoff@grandcentrix.net>
2018-02-07 22:18:42 +0100

    Extract State.elm

--- src/Main.elm
+++ src/Main.elm
@@ -7,70 +7,10 @@ import View exposing (view)
 ---- OUR IMPORTS ----
 
 import Types exposing (..)
-import GameSocket
+import State exposing (init, update, subscriptions)
 
 
 ---- MODEL ----
-
-
-init : ( Model, Cmd Msg )
-init =
-    { socket = GameSocket.initialize
-    , debugMessages = [ "Joining game room!" ]
-    , gameState = NotStarted
-    }
-        |> GameSocket.joinGameRoom "foo"
-
-
-
----- UPDATE ----
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    let
-        newModel =
-            case msg of
-                JoinedChannel response ->
-                    model |> appendMessage "Joined channel successfully!"
-
-                JoinError response ->
-                    model |> appendMessage ("Joining channel failed with message " ++ (toString response))
-
-                ChannelError response ->
-                    model
-
-                GotServerMessage _ ->
-                    model
-
-                GameStarted game ->
-                    { model | gameState = Running game }
-
-                DecodeError errorMessage ->
-                    model |> appendMessage errorMessage
-
-                _ ->
-                    model
-    in
-        newModel ! []
-
-
-appendMessage : String -> Model -> Model
-appendMessage message model =
-    { model | debugMessages = message :: model.debugMessages }
-
-
-
----- SUBS ----
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    GameSocket.listenSubscription model.socket
-
-
-
----- PROGRAM ----
 
 
 main : Program Never Model Msg
---
+++ src/State.elm
@@ -0,0 +1,64 @@
+module State exposing (init, update, subscriptions)
+
+import GameSocket
+import Types exposing (..)
+
+
+init : ( Model, Cmd Msg )
+init =
+    { socket = GameSocket.initialize
+    , debugMessages = [ "Joining game room!" ]
+    , gameState = NotStarted
+    }
+        |> GameSocket.joinGameRoom "foo"
+
+
+
+---- UPDATE ----
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    let
+        newModel =
+            case msg of
+                JoinedChannel response ->
+                    model |> appendMessage "Joined channel successfully!"
+
+                JoinError response ->
+                    model |> appendMessage ("Joining channel failed with message " ++ (toString response))
+
+                ChannelError response ->
+                    model
+
+                GotServerMessage _ ->
+                    model
+
+                GameStarted game ->
+                    { model | gameState = Running game }
+
+                DecodeError errorMessage ->
+                    model |> appendMessage errorMessage
+
+                _ ->
+                    model
+    in
+        newModel ! []
+
+
+appendMessage : String -> Model -> Model
+appendMessage message model =
+    { model | debugMessages = message :: model.debugMessages }
+
+
+
+---- SUBS ----
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    GameSocket.listenSubscription model.socket
+
+
+
+---- PROGRAM ----
